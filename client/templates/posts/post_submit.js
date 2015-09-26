ea54fc5adeac2e90aa1d4ec261c518ff5f9b02bc
Brian J Lennon <mrbrianlennon@gmail.com>
2015-09-26 15:35:12 +0100

    Commit 9-5 Validate post contents when editing.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -26,7 +26,7 @@ Template.postSubmit.events({
    Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
-        return alert(error.reason);
+        throwError(error.reason);
 
       // show this result but route anyway
       if (result.postExists)
